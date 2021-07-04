793ffd10409a5c6f6e65112fd32f3e5f7ed07ec0
Aleksey Efimov <Aleks4404@mail.ru>
2021-07-04 19:02:09 +0400

    Добавлен тест перевод средств с первой на вторую карту в файл MoneyTransferTest.java

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPage;
 
-import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Selenide.*;
 
 
 class MoneyTransferTest {
@@ -37,6 +37,29 @@ class MoneyTransferTest {
     val actual2 = dashboardPage1.getSecondCardBalance();
     val expected2 = initialBalanceFromCard - amount;
     assertEquals(expected, actual);
+    assertEquals(expected2, actual2);
+  }
+
+  @Test
+  void shouldTransferFromFirstToSecond() {
+    val amount = 100;
+    val loginPage = new LoginPage();
+    val authInfo = DataHelper.getAuthInfo();
+    val verificationPage = loginPage.validLogin(authInfo);
+    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    val dashboardPage = verificationPage.validVerify(verificationCode);
+    dashboardPage.checkHeadingYourCards();
+    val initialBalanceToCard = dashboardPage.getSecondCardBalance();
+    val initialBalanceFromCard = dashboardPage.getFirstCardBalance();
+    val transferPage = dashboardPage.validChoosePay2();
+    transferPage.checkHeadingPaymentCards();
+    transferPage.setPayCardNumber(DataHelper.getFirstCard(), amount);
+    val dashboardPage1 = transferPage.validPayCard();
+    val actual1 = dashboardPage1.getSecondCardBalance();
+    val expected1 = initialBalanceToCard + amount;
+    val actual2 = dashboardPage1.getFirstCardBalance();
+    val expected2 = initialBalanceFromCard - amount;
+    assertEquals(expected1, actual1);
     assertEquals(expected2, actual2);
   }
 }
