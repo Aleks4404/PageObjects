3bacd769796a886fa3127716a29e14e1a44abf29
Aleksey Efimov <Aleks4404@mail.ru>
2021-07-05 17:09:44 +0400

    Исправлены тесты попытки перевода больше лимита.

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -87,18 +87,11 @@ class MoneyTransferTest {
     val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
     val dashboardPage = verificationPage.validVerify(verificationCode);
     dashboardPage.checkHeadingYourCards();
-    val initialBalanceToCard = dashboardPage.getFirstCardBalance();
     val initialBalanceFromCard = dashboardPage.getSecondCardBalance();
     val transferPage = dashboardPage.validChoosePay1();
     transferPage.checkHeadingPaymentCards();
     transferPage.setPayCardNumber(DataHelper.getSecondCard(), amount);
-    val dashboardPage1 = transferPage.validPayCard();
-    val actual = dashboardPage1.getFirstCardBalance();
-    val expected = initialBalanceToCard + amount;
-    val actual2 = dashboardPage1.getSecondCardBalance();
-    val expected2 = initialBalanceFromCard - amount;
-    assertEquals(expected, actual);
-    assertEquals(expected2, actual2);
+    transferPage.validPayExtendAmount();
   }
 
   @Test
@@ -110,17 +103,10 @@ class MoneyTransferTest {
     val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
     val dashboardPage = verificationPage.validVerify(verificationCode);
     dashboardPage.checkHeadingYourCards();
-    val initialBalanceToCard = dashboardPage.getSecondCardBalance();
     val initialBalanceFromCard = dashboardPage.getFirstCardBalance();
     val transferPage = dashboardPage.validChoosePay2();
     transferPage.checkHeadingPaymentCards();
     transferPage.setPayCardNumber(DataHelper.getFirstCard(), amount);
-    val dashboardPage1 = transferPage.validPayCard();
-    val actual1 = dashboardPage1.getSecondCardBalance();
-    val expected1 = initialBalanceToCard + amount;
-    val actual2 = dashboardPage1.getFirstCardBalance();
-    val expected2 = initialBalanceFromCard - amount;
-    assertEquals(expected1, actual1);
-    assertEquals(expected2, actual2);
+    transferPage.validPayExtendAmount();
   }
 }
